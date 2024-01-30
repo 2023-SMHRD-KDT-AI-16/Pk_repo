c62d270d829110a183440a5275519bb68e3728dd
User <User@192.168.219.158>
2024-01-30 16:29:51 +0900

    은아 네번째 커밋

--- GitTest01/src/ex_01.java
+++ GitTest01/src/ex_01.java
@@ -6,7 +6,7 @@ public class ex_01 {
 		System.out.println("팀장 첫번째 커밋__");
 		System.out.println("팀원1 두번째 커밋!");
 		System.out.println("팀원2 세번째 커밋!");
-
+		System.out.println("헤헤 네번째 커밋!!");
 	
 	}
 
