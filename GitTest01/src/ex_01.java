c0d470d197d9e5a8cbe9bfa86d389d66a36a3329
User <User@192.168.219.135>
2024-01-30 16:32:11 +0900

    팀장 5번째 커밋

--- GitTest01/src/ex_01.java
+++ GitTest01/src/ex_01.java
@@ -7,6 +7,9 @@ public class ex_01 {
 		System.out.println("팀원1 두번째 커밋!");
 		System.out.println("팀원2 세번째 커밋!");
 		System.out.println("헤헤 네번째 커밋!!");
+		
+		
+		System.err.println("내일 점심은 소세지 야채 볶음밥");
 	
 	}
 
