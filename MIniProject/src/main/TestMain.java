df57f2d6abb318131b20929a08fff74d85804e7a
User <User@192.168.219.135>
2024-01-31 08:11:06 +0900

    팀장 main test

---
+++ MIniProject/src/main/TestMain.java
@@ -0,0 +1,12 @@
+package main;
+
+public class TestMain {
+
+	public static void main(String[] args) {
+		
+		// main test
+		System.out.println("test_1");
+		
+	}
+
+}
---
+++ MIniProject/src/user_view/View.java
@@ -0,0 +1,5 @@
+package user_view;
+
+public class View {
+
+}
