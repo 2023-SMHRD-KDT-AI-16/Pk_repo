bd7c154c5fd16219d36a1cc6801669374cc482dd
User <User@192.168.219.135>
2024-02-01 19:57:45 +0900

    VIEW 아스키아트 추가 팀장 커밋

---
+++ MIniProject/src/user_DTO/ScoreDTO.java
@@ -0,0 +1,58 @@
+package user_DTO;
+
+public class ScoreDTO {
+	String nickname;
+	String score;
+	int time;
+	String id;
+	
+	
+	public ScoreDTO(String nickname, String score, int time, String id) {
+		super();
+		this.nickname = nickname;
+		this.score = score;
+		this.time = time;
+		this.id = id;
+	}
+
+	@Override
+	public String toString() {
+		return "ScoreDTO [nickname=" + nickname +", score=" + score + ", time=" + time + ", id=" + id + "]";
+	}
+
+	public String getNickname() {
+		return nickname;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
+	public String getScore() {
+		return score;
+	}
+
+	public void setScore(String score) {
+		this.score = score;
+	}
+
+	public int getTime() {
+		return time;
+	}
+
+	public void setTime(int time) {
+		this.time = time;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	
+	
+
+}
